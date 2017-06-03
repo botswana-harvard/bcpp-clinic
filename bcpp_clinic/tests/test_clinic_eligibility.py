0a1c68606b4d4d6f2826660ce393243ecb519342
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-06-03 17:00:35 +0200

    test, create clinic eligibility

--- bcpp_clinic/tests/test_clinic_eligibility.py
+++ bcpp_clinic/tests/test_clinic_eligibility.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, tag
 
 from model_mommy import mommy
 
@@ -13,20 +13,22 @@ from bcpp_clinic.utils import get_clinic_member
 fake = Faker()
 
 
-class TestClinicEligibility(MemberMixin, TestCase):
+@tag('TestClinicEligibility')
+class TestClinicEligibility(TestCase):
 
     def setUp(self):
         pass
 
     def test_clinic_eligibility(self):
-#         options = {}
-#         options.update(
-#             first_name='TEST',
-#             initials='TT',
-#             report_datetime=get_utcnow(),
-#             age_in_years=22,
-#             gender=MALE)
-#         clinic_household_member = get_clinic_member(**options)
+        options = {}
+        options.update(
+            first_name='TEST',
+            initials='TT',
+            report_datetime=get_utcnow(),
+            age_in_years=22,
+            gender=MALE)
+        clinic_household_member = get_clinic_member(**options)
         mommy.make_recipe(
-            'bcpp_clinic.cliniceligibility')
+            'bcpp_clinic.cliniceligibility',
+            clinic_household_member=clinic_household_member)
         self.assertEqual(ClinicEligibility.objects.all().count(), 1)
