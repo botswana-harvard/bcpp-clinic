0fd1cadd66842d73a01a1f06f7d245df8a6293c2
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-06-06 13:59:00 +0200

    remove properties

--- bcpp_clinic/eligibility.py
+++ bcpp_clinic/eligibility.py
@@ -11,28 +11,19 @@ class AgeEvaluator:
         self.age = age
         self.adult_lower = adult_lower or app_config.eligibility_age_adult_lower
         self.adult_upper = adult_upper or app_config.eligibility_age_adult_upper
-
-    @property
-    def eligible(self):
-        """Returns True if within age range.
-        """
-        eligible = False
+        self.reason = None
+        self.eligible = None
         try:
             if self.adult_lower <= self.age <= self.adult_upper:
-                eligible = True
+                self.eligible = True
         except TypeError:
             pass
-        return eligible
 
-    @property
-    def reason(self):
-        reason = None
         if not self.eligible:
             if self.age < self.adult_lower:
-                reason = f'age<{self.adult_lower}'
+                self.reason = f'age<{self.adult_lower}'
             elif self.age > self.adult_upper:
-                reason = f'age>{self.adult_upper}'
-        return reason
+                self.reason = f'age>{self.adult_upper}'
 
 
 class IdentityEvaluator:
@@ -40,23 +31,12 @@ class IdentityEvaluator:
     def __init__(self, has_identity=None, identity=None):
         self.has_identity = has_identity
         self.identity = identity
-
-    @property
-    def eligible(self):
-        """Returns True if within age range.
-        """
-        eligible = False
+        self.eligible = None
+        self.reason = None
         if self.has_identity == YES and self.identity:
-            eligible = True
-        return eligible
-
-    @property
-    def reason(self):
-        reason = None
-        if not self.eligible:
-            if self.has_identity == NO:
-                reason = 'No valid identity.'
-        return reason
+            self.eligible = True
+        if not self.eligible and self.has_identity == NO:
+            self.reason = 'No valid identity.'
 
 
 class CitizenshipEvaluator:
@@ -66,28 +46,18 @@ class CitizenshipEvaluator:
         self.citizen = citizen
         self.legal_marriage = legal_marriage
         self.marriage_certificate = marriage_certificate
-
-    @property
-    def eligible(self):
-        """Returns True if citizen or not a citizen and has marriage certicate.
-        """
-        eligible = False
+        self.eligible = None
+        self.reason = None
         if (self.citizen == YES) or (
                 self.citizen == NO and self.marriage_certificate == YES and
-                    self.legal_marriage == YES):
-            eligible = True
-        return eligible
+                self.legal_marriage == YES):
+            self.eligible = True
 
-    @property
-    def reason(self):
-        reason = None
-        if not self.eligible:
-            if self.citizen == NO:
+        if not self.eligible and self.citizen == NO:
                 if self.legal_marriage == YES and self.marriage_certificate == NO:
-                    reason = 'Not a citizen, married to a citizen but does not have a marriage certificate.'
+                    self.reason = 'Not a citizen, married to a citizen but does not have a marriage certificate.'
                 elif self.legal_marriage == NO:
-                    reason = 'Not a citizen and not married to a citizen..'
-        return reason
+                    self.reason = 'Not a citizen and not married to a citizen..'
 
 
 class LiteracyEvaluator:
@@ -95,24 +65,15 @@ class LiteracyEvaluator:
     def __init__(self, literate=None, guardian=None):
         self.literate = literate
         self.guardian = guardian
-
-    @property
-    def eligible(self):
-        """Returns True if literate or (Not literate and guardian has YES)
-        """
-        eligible = False
+        self.eligible = None
+        self.reason = None
         if self.literate == YES or (
                 self.literate == NO and self.guardian == YES):
-            eligible = True
-        return eligible
+            self.eligible = True
 
-    @property
-    def reason(self):
-        reason = None
         if not self.eligible:
             if self.literate == NO and (not self.guardian or self.guardian == NO):
-                reason = 'Illiterate with no literate witness.'
-        return reason
+                self.reason = 'Illiterate with no literate witness.'
 
 
 class Eligibility:
@@ -120,7 +81,9 @@ class Eligibility:
     def __init__(self, age=None, literate=None, guardian=None, legal_marriage=None,
                  marriage_certificate=None, citizen=None, has_identity=None,
                  identity=None):
+
         self.age_evaluator = AgeEvaluator(age=age)
+
         self.citizenship = CitizenshipEvaluator(
             citizen=citizen, legal_marriage=legal_marriage,
             marriage_certificate=marriage_certificate)
@@ -133,12 +96,7 @@ class Eligibility:
             citizen=self.citizenship.eligible,
             has_identity=self.identity_evaluator.eligible,
             literate=self.literacy_evaluator.eligible)
-
-    @property
-    def eligible(self):
-        """Returns True if all criteria evaluate True.
-        """
-        return all(self.criteria.values())
+        self.eligible = all(self.criteria.values())
 
     @property
     def reasons(self):
