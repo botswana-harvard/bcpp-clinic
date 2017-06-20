44d29ce9e1dd211973cf2065999473356ebc341f
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-06-20 10:57:05 +0200

    bootstrap3

--- bcpp_clinic/settings.py
+++ bcpp_clinic/settings.py
@@ -194,6 +194,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, APP_NAME, 'static')
