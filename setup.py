0a6446a0c6b86bccc5b9de8a358926477573bd7b
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-08-22 12:34:34 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='bcpp-clinic',
-    version='0.1dev0',
+    version='0.1.0',
     author=u'Coulson Thabo Kgathi',
     author_email='ckgathi@gmail.com',
     packages=find_packages(),
